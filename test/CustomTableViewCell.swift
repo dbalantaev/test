56e6b8b885698cb6acc973dada983d261e2135f0
Dmitry Balantaev <dmitrybalantaev@gmail.com>
2022-03-12 14:47:48 +0600

    full done!

--- test/CustomTableViewCell.swift
+++ test/CustomTableViewCell.swift
@@ -6,56 +6,26 @@
 //
 
 import UIKit
-import SnapKit
 
 class CustomTableViewCell: UITableViewCell {
-
-    let NameTF: UITextField = {
-        let nameTextField = UITextField()
-        nameTextField.placeholder = "Имя"
-        nameTextField.borderStyle = .roundedRect
-        return nameTextField
-    }()
     
-    let AgeTF: UITextField = {
-        let ageTextField = UITextField()
-        ageTextField.placeholder = "Возраст"
-        ageTextField.borderStyle = .roundedRect
-        return ageTextField
-    }()
     
-     let delete: UIButton = {
-        let dlt = UIButton(type: .system)
-        dlt.setTitle("Удалить", for: .normal)
-        dlt.setTitleColor(UIColor(red: 57/255, green: 171/255, blue: 251/255, alpha: 1), for: .normal)
-        return dlt
-    }()
+    @IBOutlet weak var nameTF: UITextField!
     
-    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
-        super.init(style: style, reuseIdentifier: reuseIdentifier)
-        contentView.addSubview(NameTF)
-        NameTF.snp.makeConstraints { make in
-            make.left.equalToSuperview().inset(20)
-            make.height.equalTo(60)
-            make.width.equalTo(200)
-        }
-        contentView.addSubview(AgeTF)
-        AgeTF.snp.makeConstraints { make in
-            make.top.equalTo(NameTF.snp.bottom).offset(20)
-            make.left.equalToSuperview().inset(20)
-            make.height.equalTo(60)
-            make.width.equalTo(200)
-        }
-        contentView.addSubview(delete)
-        delete.snp.makeConstraints { make in
-            make.right.equalToSuperview().inset(50)
-            make.width.equalTo(100)
-            make.height.equalTo(60)
-        }
+    @IBOutlet weak var ageTF: UITextField!
+    
+    @IBOutlet weak var deleteRowBtn: UIButton!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        nameTF.borderStyle = .roundedRect
+        ageTF.borderStyle = .roundedRect
     }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
     }
 }
 
--- test/ViewController.swift
+++ test/ViewController.swift
@@ -6,122 +6,57 @@
 //
 
 import UIKit
-import SnapKit
 
 class ViewController: UIViewController {
     
     var tagCell : [String] = []
     
-    let tableView = UITableView()
-    let nameTextField = UITextField()
-    let ageTextField = UITextField()
+    @IBOutlet weak var nameTextField: UITextField!
     
+    
+    @IBOutlet weak var ageTextField: UITextField!
+    
+    @IBOutlet weak var addRowBtn: UIButton!
+    
+    @IBOutlet weak var cleanAllBtn: UIButton!
+    
+    
+    
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        let personalData = UILabel()
-        personalData.text = "Персональные данные"
-        personalData.font = UIFont.systemFont(ofSize: 20)
-        view.addSubview(personalData)
-        personalData.snp.makeConstraints { make in
-            make.left.equalTo(20)
-            make.top.equalTo(80)
-        }
-
-        nameTextField.placeholder = "Имя"
         nameTextField.borderStyle = .roundedRect
-        view.addSubview(nameTextField)
-        nameTextField.snp.makeConstraints { make in
-            make.top.equalTo(personalData.snp.bottom).offset(20)
-            make.left.equalTo(20)
-            make.height.equalTo(60)
-            make.width.equalToSuperview().inset(20)
-        }
-
-        ageTextField.placeholder = "Возраст"
         ageTextField.borderStyle = .roundedRect
-        view.addSubview(ageTextField)
-        ageTextField.snp.makeConstraints { make in
-            make.top.equalTo(nameTextField.snp.bottom).offset(20)
-            make.left.equalTo(20)
-            make.height.equalTo(60)
-            make.width.equalToSuperview().inset(20)
-
-        }
-
-        let children = UILabel()
-        children.text = "Дети (макс.5)"
-        children.font = UIFont.systemFont(ofSize: 20)
-        view.addSubview(children)
-        children.snp.makeConstraints { make in
-            make.left.equalTo(20)
-            make.top.equalTo(ageTextField.snp.bottom).offset(30)
-        }
-
-        let addChildren = UIButton(type: .system)
-        addChildren.setTitle("+ Добавить ребенка", for: .normal)
-        addChildren.setTitleColor(UIColor(red: 57/255, green: 171/255, blue: 251/255, alpha: 1), for: .normal)
-        addChildren.layer.cornerRadius = 25
-        addChildren.layer.borderWidth = 2
-        addChildren.layer.borderColor = CGColor(red: 57/255, green: 171/255, blue: 251/255, alpha: 1)
-        view.addSubview(addChildren)
-        addChildren.snp.makeConstraints { make in
-            make.right.equalTo(-20)
-            make.top.equalTo(ageTextField.snp.bottom).offset(20)
-            make.width.equalTo(200)
-            make.height.equalTo(50)
-        }
-        addChildren.addTarget(self, action: #selector(addRow), for: .touchUpInside)
-        if tagCell.count == 5 {
-            addChildren.isHidden = true
-            print("Hidden")
-        }
         
+        addRowBtn.setTitleColor(UIColor(red: 57/255, green: 171/255, blue: 251/255, alpha: 1), for: .normal)
+        addRowBtn.layer.cornerRadius = 25
+        addRowBtn.layer.borderWidth = 2
+        addRowBtn.layer.borderColor = CGColor(red: 57/255, green: 171/255, blue: 251/255, alpha: 1)
         
-        view.addSubview(tableView)
-        tableView.snp.makeConstraints { make in
-            make.top.equalTo(addChildren.snp.bottom).offset(20)
-            make.height.equalTo(400)
-            make.width.equalToSuperview()
-        }
-        tableView.register(CustomTableViewCell.self, forCellReuseIdentifier: "text")
-        tableView.delegate = self
-        tableView.dataSource = self
+        cleanAllBtn.setTitleColor(UIColor(red: 216/255, green: 63/255, blue: 42/255, alpha: 1), for: .normal)
+        cleanAllBtn.layer.cornerRadius = 25
+        cleanAllBtn.layer.borderWidth = 2
+        cleanAllBtn.layer.borderColor = CGColor(red: 216/255, green: 63/255, blue: 42/255, alpha: 1)
         
-        let clean = UIButton(type: .system)
-        clean.setTitle("Очистить", for: .normal)
-        clean.setTitleColor(UIColor(red: 216/255, green: 63/255, blue: 42/255, alpha: 1), for: .normal)
-        clean.layer.cornerRadius = 25
-        clean.layer.borderWidth = 2
-        clean.layer.borderColor = CGColor(red: 216/255, green: 63/255, blue: 42/255, alpha: 1)
-        view.addSubview(clean)
-        clean.snp.makeConstraints { make in
-            make.bottom.equalToSuperview().inset(30)
-            make.centerX.equalToSuperview()
-            make.width.equalTo(220)
-            make.height.equalTo(50)
-            }
-        clean.addTarget(self, action: #selector(cleanAll), for: .touchUpInside)
     }
     
-     @objc func addRow(_ sender: UIButton) {
-         let asd = String(tagCell.count + 1)
-         tagCell.append(asd)
-         print(tagCell)
-         tableView.insertRows(at: [IndexPath(row: tagCell.count - 1, section: 0)], with: .automatic)
-         print("add row")
-    }
     
-    @objc private func deleteRow(_ sender: UIButton) {
-        let point = sender.convert(CGPoint.zero, to: tableView)
-        guard let indexpath = tableView.indexPathForRow(at: point) else {return}
-        tagCell.remove(at: indexpath.row)
-        tableView.deleteRows(at: [IndexPath(row: indexpath.row, section: 0)], with: .fade)
+    @IBAction func addRowClick(_ sender: Any) {
+        let asd = String(tagCell.count + 1)
+        tagCell.append(asd)
         print(tagCell)
-        print("delete row")
+        tableView.insertRows(at: [IndexPath(row: tagCell.count - 1, section: 0)], with: .automatic)
+        print("add row")
+       if tagCell.count == 5 {
+           addRowBtn.isHidden = true
+       } else {
+           addRowBtn.isHidden = false
+       }
+        
     }
     
-    @objc func cleanAll() {
+    
+    @IBAction func cleanAllClick(_ sender: Any) {
         let alertController = UIAlertController(title: "Удалить все", message: "Очистить все данные?", preferredStyle: .actionSheet)
         let cancel = UIAlertAction(title: "Отмена", style: .cancel) { action in
         }
@@ -137,6 +72,16 @@ class ViewController: UIViewController {
         alertController.addAction(yes)
         self.present(alertController, animated: true, completion: nil)
     }
+    
+    
+    @IBAction func deleteRowClick(_ sender: UIButton) {
+        let point = sender.convert(CGPoint.zero, to: tableView)
+        guard let indexpath = tableView.indexPathForRow(at: point) else {return}
+        tagCell.remove(at: indexpath.row)
+        tableView.deleteRows(at: [IndexPath(row: indexpath.row, section: 0)], with: .fade)
+        print(tagCell)
+        print("delete row")
+    }
 }
 
 extension ViewController: UITableViewDelegate, UITableViewDataSource {
@@ -150,8 +95,8 @@ extension ViewController: UITableViewDelegate, UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "text", for: indexPath) as! CustomTableViewCell
-        cell.delete.addTarget(self, action: #selector(deleteRow), for: .touchUpInside)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CustomTableViewCell", for: indexPath) as! CustomTableViewCell
+//        cell.deleteRowBtn.addTarget(self, action: #selector(deleteRowClick), for: .touchUpInside)
         return cell
     }
 }
